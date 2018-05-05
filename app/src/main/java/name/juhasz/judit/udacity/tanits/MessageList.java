2c27c6a81648733747452c442230b9ceca8a13f1
Judit Juhasz <judit@juhasz.name>
2018-05-05 14:31:23 +0200

    Add an empty android studio project as base

---
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/MessageList.java
@@ -0,0 +1,13 @@
+package name.juhasz.judit.udacity.tanits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+    }
+}
