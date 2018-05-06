be4918a7e80199a0b4708af555658e653312d6c6
Judit Juhasz <judit@juhasz.name>
2018-05-07 00:54:59 +0200

    Add toolbar to the layout and set as the action bar

--- app/src/main/java/name/juhasz/judit/udacity/tanits/MessageList.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/MessageList.java
@@ -4,6 +4,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MessageList extends AppCompatActivity {
@@ -14,6 +15,9 @@ public class MessageList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
