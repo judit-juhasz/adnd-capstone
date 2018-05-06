e21fa30e5979f1ca5569a54d99d2410be0911dee
Judit Juhasz <judit@juhasz.name>
2018-05-07 01:00:03 +0200

    Add the navigation drawer button

--- app/src/main/java/name/juhasz/judit/udacity/tanits/MessageList.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/MessageList.java
@@ -2,6 +2,7 @@ package name.juhasz.judit.udacity.tanits;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,6 +19,9 @@ public class MessageList extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
