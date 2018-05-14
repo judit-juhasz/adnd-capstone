1e87faf5782acb31088567c25838276b00698f92
Judit Juhasz <judit@juhasz.name>
2018-05-14 06:34:31 +0200

    Prepare the fragment for the Messages menuitem

---
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/MessagesFragment.java
@@ -0,0 +1,22 @@
+package name.juhasz.judit.udacity.tanits;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MessagesFragment extends Fragment {
+
+    public MessagesFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+        return rootView;
+    }
+}
