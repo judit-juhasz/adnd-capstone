df725ab7ec8b40d662c1dd9b0bd185f3ad922e88
Judit Juhasz <judit@juhasz.name>
2018-05-14 06:49:47 +0200

    Prepare a fragment for About menuitem

---
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/AboutFragment.java
@@ -0,0 +1,23 @@
+package name.juhasz.judit.udacity.tanits;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        return rootView;
+    }
+
+}
