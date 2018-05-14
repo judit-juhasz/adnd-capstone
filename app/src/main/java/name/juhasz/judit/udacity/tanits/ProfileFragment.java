046bcdda4c25f20eb5c2b8768a43558615e318ad
Judit Juhasz <judit@juhasz.name>
2018-05-14 06:44:33 +0200

    Prepare the fragment for Profile menuitem

---
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/ProfileFragment.java
@@ -0,0 +1,22 @@
+package name.juhasz.judit.udacity.tanits;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        return rootView;
+    }
+}
