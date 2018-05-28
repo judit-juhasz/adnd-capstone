75d613af57acbec32cc53ad28e7ba392de2e0fb2
Judit Juhasz <judit@juhasz.name>
2018-05-28 07:07:16 +0200

    Add authentication to the project (login)

--- app/src/main/java/name/juhasz/judit/udacity/tanits/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/MainActivity.java
@@ -1,6 +1,7 @@
 package name.juhasz.judit.udacity.tanits;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
@@ -12,14 +13,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements MessageAdapter.OnClickListener {
 
+    public static final int RC_SIGN_IN = 1;
+    public static final String ANONYMOUS = "anonymous";
     public static int navigationItemIndex = 0;
     private static final String TAG_MESSAGES = "messages";
     private static final String TAG_PROFILE = "profile";
     private static final String TAG_ABOUT_US = "about_us";
     public static String CURRENT_TAG = TAG_MESSAGES;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private String mUsername;
 
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
@@ -33,6 +47,27 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -55,6 +90,43 @@ public class MainActivity extends AppCompatActivity implements MessageAdapter.On
             loadHomeFragment();
         }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+    }
+
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
+    }
+
+
 
     private void loadActionBarDrawerToggle(DrawerLayout layout) {
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
