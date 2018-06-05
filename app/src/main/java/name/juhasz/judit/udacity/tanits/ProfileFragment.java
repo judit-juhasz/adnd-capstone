5bcc72600ccf7af9763b9d02cf1b9c7fee4179b0
Judit Juhasz <judit@juhasz.name>
2018-06-05 18:56:12 +0200

    Show the stored profile information before edition

--- app/src/main/java/name/juhasz/judit/udacity/tanits/ProfileFragment.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/ProfileFragment.java
@@ -1,6 +1,7 @@
 package name.juhasz.judit.udacity.tanits;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -13,7 +14,10 @@ import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Calendar;
 
@@ -54,6 +58,54 @@ public class ProfileFragment extends Fragment {
                         .setValue(mBirthdateOfChildEditText.getText().toString());
             }
         });
+
+        mFirebaseDatabase.getReference("profiles/" +  currentFirebaseUser.getUid() + "/displayname")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        final String value = dataSnapshot.getValue(String.class);
+                        if (null != value) {
+                            mDisplayNameEditText.setText(value);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        mFirebaseDatabase.getReference("profiles/" +  currentFirebaseUser.getUid() + "/email")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        final String value = dataSnapshot.getValue(String.class);
+                        if (null != value) {
+                            mEmailEditText.setText(value);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        mFirebaseDatabase.getReference("profiles/" +  currentFirebaseUser.getUid() + "/birthday")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        final String value = dataSnapshot.getValue(String.class);
+                            if (null != value) {
+                                mBirthdateOfChildEditText.setText(value);
+                            }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
         return rootView;
     }
