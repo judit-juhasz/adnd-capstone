a735f7a4b5df739cab5a79a2c5707cf427809eea
Judit Juhasz <judit@juhasz.name>
2018-06-06 02:11:12 +0200

    Use floating label EditText instead of EditText

--- app/src/main/java/name/juhasz/judit/udacity/tanits/ProfileFragment.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/ProfileFragment.java
@@ -4,7 +4,10 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,9 +33,11 @@ public class ProfileFragment extends Fragment {
     private static final String TAG = ProfileFragment.class.getSimpleName();
 
     private FirebaseDatabase mFirebaseDatabase;
-    private EditText mDisplayNameEditText;
+    private EditText mNameEditText;
     private EditText mEmailEditText;
     private EditText mBirthdateOfChildEditText;
+    private TextInputLayout inputLayoutName;
+    private TextInputLayout inputLayoutBirthdateOfChild;
     private Button mSaveButton;
 
     public ProfileFragment() {
@@ -43,9 +49,11 @@ public class ProfileFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
         mSaveButton = rootView.findViewById(R.id.button_save);
-        mDisplayNameEditText = rootView.findViewById(R.id.et_display_name);
+        mNameEditText = rootView.findViewById(R.id.et_name);
         mEmailEditText = rootView.findViewById(R.id.et_email);
         mBirthdateOfChildEditText = rootView.findViewById(R.id.et_birthdate_of_child);
+        inputLayoutName = rootView.findViewById(R.id.input_layout_name);
+        inputLayoutBirthdateOfChild = rootView.findViewById(R.id.input_layout_birthdate);
 
         final Calendar myCalendar = Calendar.getInstance();
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -73,6 +81,8 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        mNameEditText.addTextChangedListener(new MyTextWatcher(mNameEditText));
+        mBirthdateOfChildEditText.addTextChangedListener(new MyTextWatcher(mBirthdateOfChildEditText));
 
         final FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
@@ -80,8 +90,13 @@ public class ProfileFragment extends Fragment {
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                if (isInvalidConfig()) {
+                    return;
+                }
+
                 final UserProfile userProfile =
-                        new UserProfile(mDisplayNameEditText.getText().toString(),
+                        new UserProfile(mNameEditText.getText().toString(),
                                 mEmailEditText.getText().toString(),
                                 mBirthdateOfChildEditText.getText().toString());
                 mFirebaseDatabase.getReference("profiles/" +  currentFirebaseUser.getUid())
@@ -96,10 +111,11 @@ public class ProfileFragment extends Fragment {
                         final UserProfile user = dataSnapshot.getValue(UserProfile.class);
                         if (null != user) {
                             if (null != user.getName()) {
-                                mDisplayNameEditText.setText(user.getName());
+                                mNameEditText.setText(user.getName());
                             }
                             if (null != user.getEmail()) {
                                 mEmailEditText.setText(user.getEmail());
+                                mEmailEditText.setEnabled(false);
                             }
                             if (null != user.getChildBirthdate()) {
                                 mBirthdateOfChildEditText.setText(user.getChildBirthdate());
@@ -114,5 +130,67 @@ public class ProfileFragment extends Fragment {
                 });
 
         return rootView;
+    }
+
+    private boolean validateName() {
+        if (mNameEditText.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.error_message_name));
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private boolean validateBirthdate() {
+        if (mBirthdateOfChildEditText.getText().toString().trim().isEmpty()) {
+            inputLayoutBirthdateOfChild.setError(getString(R.string.error_message_birthdate));
+            registerForContextMenu(mBirthdateOfChildEditText);
+            return false;
+        } else {
+            inputLayoutBirthdateOfChild.setErrorEnabled(false);
+        }
+        return true;
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.et_name:
+                    validateName();
+                    break;
+                case R.id.et_birthdate_of_child:
+                    validateBirthdate();
+                    break;
+            }
+        }
+    }
+
+    public boolean isInvalidConfig() {
+        final boolean isNameCorrect = !mNameEditText.getText().toString().isEmpty();
+        final boolean isBirthdateOfChildCorrect = !mBirthdateOfChildEditText.getText().toString().isEmpty();
+
+        if (!isNameCorrect && !isBirthdateOfChildCorrect) {
+            Toast.makeText(getContext(), getString(R.string.error_message_missing_name_birthdate),
+                    Toast.LENGTH_LONG).show();
+            return true;
+        } else if (!isNameCorrect) {
+            Toast.makeText(getContext(), getString(R.string.error_message_name), Toast.LENGTH_LONG).show();
+            return true;
+        } else if (!isBirthdateOfChildCorrect) {
+            Toast.makeText(getContext(), getString(R.string.error_message_birthdate), Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return false;
     }
 }
