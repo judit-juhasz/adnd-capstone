0de6cbe37341c7dc6a35fb4bbc0784f0351aea6e
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2018-06-14 00:25:09 +0200

    Use Coordinator Layout in Profile fragment

--- app/src/main/java/name/juhasz/judit/udacity/tanits/AboutFragment.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/AboutFragment.java
@@ -6,8 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
+
+    private TextView mDisplayTextView;
 
     public AboutFragment() {
     }
@@ -17,6 +20,13 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        mDisplayTextView = rootView.findViewById(R.id.tv_text_one);
+        String displaytext = getResources().getString(R.string.text_one_about)
+                + getResources().getString(R.string.text_two_about)
+                + getResources().getString(R.string.text_one_about)
+                + getResources().getString(R.string.text_one_about);
+        mDisplayTextView.setText(displaytext);
         return rootView;
     }
 
