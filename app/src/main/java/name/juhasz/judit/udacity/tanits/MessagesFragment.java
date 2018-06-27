2258cdafb09dba171c3bb4f52c43e5fdb12a6516
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2018-06-27 21:53:09 +0200

    Clean dead code

--- app/src/main/java/name/juhasz/judit/udacity/tanits/MessagesFragment.java
+++ app/src/main/java/name/juhasz/judit/udacity/tanits/MessagesFragment.java
@@ -1,8 +1,6 @@
 package name.juhasz.judit.udacity.tanits;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.github.clans.fab.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseError;
 
